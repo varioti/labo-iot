fcf949c499f33b3ca098f87517fad114e4790d13
21inno <innoye2000@gmail.com>
2022-11-08 14:34:58 +0100

    window + route function

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template, redirect
 from flask import url_for, request
 
+
 ##########
 # ROUTES #
 ##########
@@ -11,8 +12,17 @@ def hello_world():
     return render_template("index.html")
 
 
+@app.route("/window/", methods=["GET", "POST"])
+def window(state=False):
+    state_window = state
+    if request.method == 'POST':
+        return redirect(url_for("window"))
+
+    return render_template("window.html", state_window=state_window)
+
+
 #######
 # RUN #
 #######
 if __name__ == "__main__":
-        app.run()
+    app.run()
