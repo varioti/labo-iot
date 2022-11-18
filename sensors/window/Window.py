e12d6c6849c8d684e0f28d8af367bf57b5c1ec3b
valatt19 <valatt19@gmail.com>
2022-11-18 12:01:42 +0100

    better humidity handling

--- sensors/window/Window.py
+++ sensors/window/Window.py
@@ -7,6 +7,7 @@
 from phidgets.motor import Motor
 from phidgets.hub import Hub
 import time
+from datetime import datetime, timedelta
 
 # Window actions
 NOT = "nothing"
@@ -20,6 +21,8 @@ class Window:
         self.is_open = False
         self.mode_auto = True
         self.state = []
+        self.last_dehum = None
+        self.cooldown = False
 
         self.temp_desired = temp_desired
         self.max_hum = max_hum
@@ -64,18 +67,38 @@ class Window:
             repr = repr + " and " + self.state[i+1]
 
         return repr
+    
+    # Set last dehum to now (init cooldown)
+    def set_last_dehum(self):
+        self.last_dehum = datetime.today()
+
+    # Reset cooldown to False if there is 12 hours from last dehum
+    def check_cooldown_dehum(self):
+        if not self.last_dehum is None:
+            if (datetime.today() - self.last_dehum).total_seconds()/3600 >= 12:
+                self.cooldown = False
+
+    # Check if time before closing window when DEHUM is finished and start cooldown
+    def check_end_dehum(self, delta = 10):
+        if not self.last_dehum is None:
+            if (datetime.today() - self.last_dehum).total_seconds()/60 >= delta:
+                self.set_last_dehum()
+                self.cooldown = True
+
 
 
     # Define the behavior of the window in function of the measures and its current state
     def behavior(self):
+        # Init
         self.state = []
-
         self.get_measures()
         
         # Reasons why whe should open the window
         too_cold_in = self.temp_in < self.temp_desired and self.temp_out > self.temp_in
         too_hot_in = self.temp_in > self.temp_desired and self.temp_out < self.temp_in
-        too_hum_in = self.humidity > self.max_hum
+
+        self.check_end_dehum(10) # Check if 10 minutes passed since window open for DEHUM (if yes cooldown started)
+        too_hum_in = self.humidity > self.max_hum and not self.cooldown # And window will not stay open
 
         if too_cold_in:
             self.state.append(HEAT)
@@ -101,6 +124,10 @@ class Window:
             if len(self.state) > 0 :
                 self.open()
                 print(f"Window opened for {self.repr_state()} (Temp inside = {self.temp_in}°C | Temp outside = {self.temp_out}°C | Humidity = {self.humidity})")
+
+                # If window is only dehumidifiating the room we save the time in order to limit the time in which the window will stay open
+                if self.state == [DEHUM]:
+                    self.set_last_dehum()
 
 
 # Initiate window
